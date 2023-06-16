8542ed6daa4696be54c85d5246502952bea8f65c
朱世俊 <qq1034816916@163.com>
2023-06-16 10:30:42 +0800

    👏feat:[unplugin-vue-components]

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
                 enabled: true,
             },
         }),
+        Components({}),
         uni(),
     ],
     resolve: {
